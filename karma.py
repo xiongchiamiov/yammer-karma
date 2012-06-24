edfc6d20797faec40ff61c05b5cdbe86e559675c
xiongchiamiov <xiong.chiamiov@gmail.com>
2012-06-24 12:51:42 -0700

    go through pagination of both users and liked messages

--- karma.py
+++ karma.py
@@ -6,36 +6,63 @@
 
 import json
 import oauth2 as oauth
+import pickle
 import urlparse
 from collections import defaultdict
 from time import sleep
 
 from local_settings import key, secret, token, tokenSecret
 
+USERS_PER_PAGE = 50
+
 consumer = oauth.Consumer(key=key, secret=secret)
 token = oauth.Token(token, tokenSecret)
 client = oauth.Client(consumer, token)
 
-url = 'https://www.yammer.com/api/v1/messages.json'
-response, content = client.request(url, 'GET')
+userMapping = {}
+likeCounts = defaultdict(int)
 
-users = defaultdict(int)
-
-messages = json.loads(content)['messages']
-for message in messages:
-	print 'user %s' % message['sender_id']
-	print message['body']['plain']
-	print '%s likes' % message['liked_by']['count']
-	print
-	users[message['sender_id']] += message['liked_by']['count']
-
-print users
-
-for user, likes in users.iteritems():
-	url = 'https://www.yammer.com/api/v1/users/%s.json' % user
+# Go through each page of users.  Assume the number of employees is never
+# evenly-divisible by USERS_PER_PAGE.
+while len(userMapping) % USERS_PER_PAGE == 0:
+	page = (len(userMapping) + USERS_PER_PAGE) / USERS_PER_PAGE
+	url = 'https://www.yammer.com/api/v1/users.json?page=%s' % page
 	response, content = client.request(url, 'GET')
-	user = json.loads(content)
-	print '%s likes - %s' % (likes, user['full_name'])
-	print user['mugshot_url']
-	sleep(1) # For rate-limiting
+	users = json.loads(content)
+	for user in users:
+		print "Looking at %s's likes..." % user['full_name']
+		userMapping[user['id']] = user['full_name']
+
+		baseMessageUrl = 'https://www.yammer.com/api/v1/messages/liked_by/' \
+		               + str(user['id']) + '.json'
+
+		# Paginate through liked messages
+		messages = None
+		while True:
+			url = baseMessageUrl
+			if messages:
+				url += '?older_than=%s' % messages[-1]['id']
+			response, content = client.request(url, 'GET')
+			messages = json.loads(content)['messages']
+			if not messages:
+				print 'Reached end of liked messages.'
+				break
+			
+			print 'Liked %s messages:' % len(messages)
+			for message in messages:
+				print 'user %s' % message['sender_id']
+				print message['body']['plain']
+				print '%s likes' % message['liked_by']['count']
+				print
+				# No matter how many times this message has been liked, we only
+				# want to increment for this user so we don't double-count.
+				likeCounts[message['sender_id']] += 1
+				sleep(1) # For rate-limiting
+
+# Cache this for our web view
+pickle.dump(userMapping, open('userMapping.pickle', 'wb'))
+pickle.dump(likeCounts, open('likeCounts.pickle', 'wb'))
+
+for user, likes in likeCounts.iteritems():
+	print '%s likes - %s' % (likes, userMapping[user])
 
